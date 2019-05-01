1702219771327ebfc4bffafe1f20507dcd40eea6
Gillian <gilliansabdao@gmail.com>
2019-05-01 14:53:28 +1200

    Redirected items to product pages + added test product pages

--- routes/product_routes.js
+++ routes/product_routes.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET product page. */
 router.get('/product/:id', function(req, res, next) {
-  res.render('products/product_info');
+  if(req.params.id == 1){
+    res.render('products/test_tree');
+  } else if(req.params.id == 2){
+    res.render('products/test_supply');
+  } else {
+    res.render('products/product_info');
+  }
 });
 
 module.exports = router;
