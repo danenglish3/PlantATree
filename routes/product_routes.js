208a5f9ee876e66baf876b50f7975642ce4fc952
Gillian <gilliansabdao@gmail.com>
2019-05-07 18:25:30 +1200

    added hard-coded test product pages

--- routes/product_routes.js
+++ routes/product_routes.js
@@ -8,21 +8,36 @@ router.get('/product/:id', function(req, res, next) {
   con.query("SELECT * FROM PRODUCT", function(err, result){
     if (err) throw err;
     var id = req.params.id;
-    if(id < result.length){
+
+    //Blocked out for presentation reasons, this is how the pages are supposed to operate
+    /*if(id < result.length){
       var name = result[id].product_name;
       var price = '$' + result[id].product_price;
       var description = result[id].product_description;
 
       res.render('products/product_info', {name: name, price: price, description: description});
     } else {
-      
-      //Test page will change this later
       var name = 'You';
       var price = 'are not';
       var description = 'supposed to be here';
 
       res.render('products/product_info', {name: name, price: price, description: description});
-    } 
+    } */
+    
+    //Using this for presentation reasons, to make index look nice and look like we have something to sell
+    if(id == 1){
+      res.render('products/test_product1');
+    } else if(id == 2){
+      res.render('products/test_product2');
+    } else if(id == 3){
+      res.render('products/test_product3');
+    } else if(id == 4){
+      res.render('products/test_product4');
+    } else if(id == 5){
+      res.render('products/test_product5');
+    } else if(id == 6){
+      res.render('products/test_product6');
+    }
   });
 });
 
