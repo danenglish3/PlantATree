203cc5b653805966fdfe2b13f0d673dc8b23d51d
recallm <42362641+recallm@users.noreply.github.com>
2019-05-26 13:29:02 +1200

    Testing ClearDB for heroku deploy
    Changed the database.js info to test DB deployment with heroku

--- database.js
+++ database.js
@@ -2,10 +2,10 @@ var mysql = require('mysql');
 
 // Change this info to what your local DB connection info is
 var connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'password',
-    database: 'plantatree',
+    host: 'us-cdbr-iron-east-02.cleardb.net',
+    user: 'b88f5293251354',
+    password: 'cd74d17e',
+    database: 'heroku_92d94b814b5bd78',
 });
 
 // Establishing connection to database using relevant information
